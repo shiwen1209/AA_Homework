eef13485a02570e238f1e5c641f25a127fa8da99
Wendy Shi <whshiwen@gmail.com>
2022-04-27 22:06:09 -0700

    Second submission W2D3

--- W2D3/mastermind_project/lib/code.rb
+++ W2D3/mastermind_project/lib/code.rb
@@ -6,4 +6,72 @@ class Code
     "Y" => :yellow
   }
 
+  def self.valid_pegs?(arr)
+    arr.all? {|char| POSSIBLE_PEGS.keys.include?(char.upcase)}
+  end
+
+  attr_reader :pegs
+  def initialize(arr)
+    if !Code.valid_pegs?(arr)
+      raise ArgumentError.new"not valid arr"
+    end
+    @pegs = arr.map {|char| char.upcase}
+  end
+
+  def self.random(len)
+    arr = []
+    while arr.length < len
+      arr<< POSSIBLE_PEGS.keys.sample
+    end
+    code = Code.new(arr)
+  end
+
+  def length
+    @pegs.length
+  end
+
+  def self.from_string(string)
+    arr = string.split("")
+    Code.new(arr)
+  end
+
+  def [](index)
+    @pegs[index]
+  end
+
+  def num_exact_matches(code)
+    #compare code.pegs with @pegs
+    num = 0 
+    code.pegs.each_with_index do |char, i|
+      num = num + 1 if char == @pegs[i]
+    end
+    return num
+  end
+
+  def num_near_matches(code)
+    num = 0
+    mappedj = []
+    code.pegs.each_with_index do |char1, i|
+      @pegs.each_with_index do |char2, j|
+        if char1 == char2 && i == j
+          mappedj<< j
+        elsif char1 == char2 && char1 != @pegs[i] && char2 != code.pegs[j] && !mappedj.include?(j)
+          num = num + 1
+          mappedj<< j
+          break
+        end
+      end
+    end
+    return num
+  end
+
+  def ==(code)
+    if code.pegs == @pegs
+      return true
+    else
+      return false
+    end
+  end
+  
+
 end
--- W2D3/mastermind_project/lib/mastermind.rb
+++ W2D3/mastermind_project/lib/mastermind.rb
@@ -1,5 +1,22 @@
 require_relative "code"
 
 class Mastermind
+    def initialize(len)
+        @len = len
+        @secret_code = Code.random(@len)
+    end
+
+    def print_matches(code)
+        puts "Exect match: #{code.num_exact_matches(@secret_code)}"
+        puts "Near match #{code.num_near_matches(@secret_code)}"
+    end
+
+    def ask_user_for_guess
+        puts "Enter a code"
+        str = gets.chomp
+        new_code = Code.from_string(str)
+        print_matches(new_code)
+        new_code == @secret_code
+    end
 
 end
